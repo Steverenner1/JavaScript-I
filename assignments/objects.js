79ca4ee0aee9276024123ebe4aac7258d2c46aa9
Steve Renner <che1111@hotmail.com>
2019-08-19 15:49:34 -0700

    4pm turn in

--- assignments/objects.js
+++ assignments/objects.js
@@ -59,7 +59,6 @@ const intern5 = {
 intern1["name"]
 console.log(intern1["name"])
 
-
 // Kennan's ID
 intern2["id"]
 console.log(intern2["id"])
@@ -77,10 +76,22 @@ intern5["gender"]
 console.log(intern5["gender"])
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+
+intern2.speak = function() {
+  return "Hello, my name is Kennan!";
+}
+
+console.log(intern2.speak());
+
+
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+intern5.multiplyNums = function (a,b) {
+  let product = a * b;
+  return product;
+}
+
+console.log(intern5.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
